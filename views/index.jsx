b7771e12714727d35b03ff645b42c0394ea8c57b
ecdavis15 <davisethan93@gmail.com>
2017-02-25 21:30:51 -0800

    Content centered

--- views/index.jsx
+++ views/index.jsx
@@ -1,4 +1,5 @@
 var React = require('react')
+var ReactDOM = require('react-dom')
 var Solution = require('./solution')
 
 module.exports = React.createClass({
@@ -7,15 +8,16 @@ module.exports = React.createClass({
 
     return (
       <div>
-        <h2>Choose from 438984 Triangle Solo Noble solutions</h2>
-        <form method='post' action='/solution'>
-          <input type='text' name='solution' placeholder='solution' />
-          <br />
-          <input type='submit' />
-        </form>
-        {
-          solution ? <Solution solution={solution} /> : null
-        }
+        <center>
+          <h2>Choose from 438984 Triangle Solo Noble solutions</h2>
+          <form method='post' action='/solution'>
+            <input type='text' name='solution' placeholder='solution' />
+            <input type='submit' />
+          </form>
+          {
+            solution ? <Solution solution={solution} /> : null
+          }
+        </center>
       </div>
     )
   }
